94c8968d96982f79aed26f3deb96c2ce8a6a61f3
raviasahara <tbs.raviashara@gmail.com>
2020-02-14 15:26:35 +0530

    Change base url.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   // baseURL: "http://54.244.98.247",
-  baseURL: "https://vafalive.com.au",
+  // baseURL: "https://vafalive.com.au",
+  baseURL: 'https://yjfllive.com.au',
   amazonaws: 'https://s3.us-west-2.amazonaws.com/vafas3'
 };
